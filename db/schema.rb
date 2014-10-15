8ff59827264b45c53319b7fc5c5ba781e1685789
David Fisher <tibbon@gmail.com>
2014-10-15 14:23:33 -0400

    Added delegate method to Pet model

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141015181607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: true do |t|
+    t.string  "name"
+    t.integer "user_id"
+  end
+
+  add_index "pets", ["user_id"], name: "index_pets_on_user_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string "name"
+  end
 
 end
